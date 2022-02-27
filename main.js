bdd11c3337c5ce4075332091632850506a796e83
jeromechrishtopher <85341703+jeromechrishtopher@users.noreply.github.com>
2022-02-27 13:47:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+function preload(){
+    
+}
+function setup(){
+    canvas=createCanvas(700,600);
+    canvas.parent('canvas');
+    video=createCapture(VIDEO);
+    video.size(700,600);
+    video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("psoenet is initialixed");
+}
 
